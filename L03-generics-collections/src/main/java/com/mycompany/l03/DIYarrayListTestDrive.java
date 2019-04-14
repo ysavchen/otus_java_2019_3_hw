8045d2adca7c6fb8253c6f795bb8b4b04f8bd2ea
ysavchen <yura_savchenko@outlook.com>
2019-04-14 22:22:14 +0300

    added sout for list

--- L03-generics-collections/src/main/java/com/mycompany/l03/DIYarrayListTestDrive.java
+++ L03-generics-collections/src/main/java/com/mycompany/l03/DIYarrayListTestDrive.java
@@ -12,6 +12,7 @@ public class DIYarrayListTestDrive {
         if (Collections.addAll(intList,
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)) {
+            intList.forEach(System.out::println);
             System.out.println("Collections.addAll() - passed");
         }
     }
