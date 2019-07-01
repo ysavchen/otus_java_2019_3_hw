fe8d13bec88513ad2f3c9d581d04142d3d24ada5
ysavchen <yura_savchenko@outlook.com>
2019-07-01 23:26:56 +0300

    added test for account

--- L17-jdbc/src/main/java/com/mycompany/executor/JdbcTemplateImpl.java
+++ L17-jdbc/src/main/java/com/mycompany/executor/JdbcTemplateImpl.java
@@ -6,68 +6,27 @@ import com.mycompany.exceptions.NoIdFoundException;
 import com.mycompany.exceptions.SeveralIdsFoundException;
 
 import java.lang.reflect.Field;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class JdbcTemplateImpl implements JdbcTemplate {
 
-    private static final String URL = "jdbc:h2:mem:";
+    private final Connection connection;
 
-    private Connection connection;
-
-    private Connection getConnection() {
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(URL);
-                connection.setAutoCommit(false);
-                createTables(connection);
-            } catch (SQLException ex) {
-                System.out.println("Connection is not created: " + ex.toString());
-                throw new ExceptionInInitializerError(ex);
-            }
-        }
-        return connection;
-    }
-
-    private void createTables(Connection connection) throws SQLException {
-        PreparedStatement psUser = connection.prepareStatement(
-                "create table User(" +
-                        "id bigint(20) NOT NULL auto_increment, " +
-                        "name varchar(255), " +
-                        "age int(3))");
-        PreparedStatement psAccount = connection.prepareStatement(
-                "create table Account(" +
-                        "no bigint(20) NOT NULL auto_increment, " +
-                        "type varchar(255), " +
-                        "rest number)");
-
-        try (psUser; psAccount) {
-            psUser.executeUpdate();
-            psAccount.executeUpdate();
-        }
+    public JdbcTemplateImpl(Connection connection) {
+        this.connection = connection;
     }
 
     @Override
     public void create(Object object) throws SQLException {
         Field[] fields = object.getClass().getDeclaredFields();
-        int numIds = 0;
-        for (var field : fields) {
-            field.setAccessible(true);
-            if (field.getAnnotation(Id.class) != null) {
-                numIds++;
-            }
-        }
+        checkIdPresent(object, fields);
 
-        if (numIds == 0) {
-            throw new NoIdFoundException("Entity does not have a field with @Id - " + object);
-        }
-        if (numIds > 1) {
-            throw new SeveralIdsFoundException("Entity have several fields with @Id - " + object);
-        }
-
-        Connection connection = getConnection();
         DbExecutor<?> executor = new DbExecutorImpl<>(connection);
         String table = object.getClass().getSimpleName();
         String columns = Stream.of(fields)
@@ -88,13 +47,100 @@ public class JdbcTemplateImpl implements JdbcTemplate {
                 "insert into " + table + "(" + columns + ")" +
                         " values (?, ?, ?)", paramsSetter);
         connection.commit();
-
-        //todo: should connection be closed here?
-        //connection.close();
     }
 
 
-    public void setParameter(PreparedStatement pst, int idx, Object object) throws SQLException {
+    @Override
+    public void update(Object object) throws SQLException {
+        Field[] fields = object.getClass().getDeclaredFields();
+        checkIdPresent(object, fields);
+        DbExecutor<?> executor = new DbExecutorImpl<>(connection);
+
+        String table = object.getClass().getSimpleName();
+        StringBuilder update = new StringBuilder();
+        update.append("set ");
+        for (int i = 0; i < fields.length; i++) {
+            String name = fields[i].getName();
+            if (i > 0) {
+                update.append(", ");
+            }
+            update.append(name).append(" = ?");
+        }
+
+        Consumer<PreparedStatement> paramsSetter = pst -> {
+            for (int idx = 0; idx < fields.length; idx++) {
+                try {
+                    setParameter(pst, idx + 1, fields[idx].get(object));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+
+        executor.insertRecord(
+                "update " + table + " " + update, paramsSetter);
+        connection.commit();
+    }
+
+
+    @Override
+    public <T> T load(long id, Class<T> clazz) throws SQLException {
+        Object object = ReflectionUtils.instantiate(clazz);
+        Field[] fields = object.getClass().getDeclaredFields();
+        checkIdPresent(object, fields);
+        DbExecutor<T> executor = new DbExecutorImpl<>(connection);
+
+        String table = object.getClass().getSimpleName();
+        String columns = Stream.of(fields)
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
+
+        T entity = executor.selectRecord(
+                "select " + columns + " from " + table + " where id  = ?", id,
+                resultSet -> {
+                    try {
+                        if (resultSet.next()) {
+                            int idx = 1;
+                            for (Field field : fields) {
+                                field.setAccessible(true);
+                                try {
+                                    Object value = getValue(resultSet, idx, field.getType());
+                                    field.set(object, value);
+                                    idx++;
+                                } catch (Exception ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                            return clazz.cast(object);
+                        }
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                    return null;
+                });
+
+        connection.commit();
+        return entity;
+    }
+
+    private void checkIdPresent(Object object, Field[] fields) {
+        int numIds = 0;
+        for (var field : fields) {
+            field.setAccessible(true);
+            if (field.getAnnotation(Id.class) != null) {
+                numIds++;
+            }
+        }
+
+        if (numIds == 0) {
+            throw new NoIdFoundException("Entity does not have a field with @Id - " + object);
+        }
+        if (numIds > 1) {
+            throw new SeveralIdsFoundException("Entity have several fields with @Id - " + object);
+        }
+    }
+
+    private void setParameter(PreparedStatement pst, int idx, Object object) throws SQLException {
         if (object == null) {
             return;
         }
@@ -122,106 +168,6 @@ public class JdbcTemplateImpl implements JdbcTemplate {
         if (object.getClass() == Boolean.class || object.getClass() == boolean.class) {
             pst.setBoolean(idx, (Boolean) object);
         }
-    }
-
-    @Override
-    public void update(Object object) throws SQLException {
-        Field[] fields = object.getClass().getDeclaredFields();
-        int numIds = 0;
-        for (var field : fields) {
-            field.setAccessible(true);
-            if (field.getAnnotation(Id.class) != null) {
-                numIds++;
-            }
-        }
-
-        if (numIds == 0) {
-            throw new NoIdFoundException("Entity does not have a field with @Id - " + object);
-        }
-        if (numIds > 1) {
-            throw new SeveralIdsFoundException("Entity have several fields with @Id - " + object);
-        }
-
-        Connection connection = getConnection();
-        DbExecutor<?> executor = new DbExecutorImpl<>(connection);
-        String table = object.getClass().getSimpleName();
-        StringBuilder update = new StringBuilder();
-        update.append("set ");
-        for (int i = 0; i < fields.length; i++) {
-            String name = fields[i].getName();
-            if (i > 0) {
-                update.append(", ");
-            }
-            update.append(name).append(" = ?");
-        }
-
-        Consumer<PreparedStatement> paramsSetter = pst -> {
-            for (int idx = 0; idx < fields.length; idx++) {
-                try {
-                    setParameter(pst, idx + 1, fields[idx].get(object));
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        };
-
-
-        executor.insertRecord(
-                "update " + table + " " + update, paramsSetter);
-        connection.commit();
-    }
-
-    @Override
-    public <T> T load(long id, Class<T> clazz) throws SQLException {
-        Connection connection = getConnection();
-        DbExecutor<T> executor = new DbExecutorImpl<>(connection);
-
-        Object object = ReflectionUtils.instantiate(clazz);
-
-        Field[] fields = object.getClass().getDeclaredFields();
-        int numIds = 0;
-        for (var field : fields) {
-            field.setAccessible(true);
-            if (field.getAnnotation(Id.class) != null) {
-                numIds++;
-            }
-        }
-
-        if (numIds == 0) {
-            throw new NoIdFoundException("Entity does not have a field with @Id - " + object);
-        }
-        if (numIds > 1) {
-            throw new SeveralIdsFoundException("Entity have several fields with @Id - " + object);
-        }
-
-        String table = object.getClass().getSimpleName();
-        String columns = Stream.of(fields)
-                .map(Field::getName)
-                .collect(Collectors.joining(", "));
-
-
-        T entity = executor.selectRecord("select " + columns + " from " + table + " where id  = ?", id, resultSet -> {
-            try {
-                if (resultSet.next()) {
-                    int idx = 1;
-                    for (int i = 0; i < fields.length; i++) {
-                        fields[i].setAccessible(true);
-                        try {
-                            Object value = getValue(resultSet, idx, fields[i].getType());
-                            fields[i].set(object, value);
-                            idx++;
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                    return clazz.cast(object);
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-            return null;
-        });
-        return entity;
     }
 
     private Object getValue(ResultSet rs, int idx, Class<?> typeOfField) throws SQLException {
--- L17-jdbc/src/test/java/com/mycompany/OrmTests.java
+++ L17-jdbc/src/test/java/com/mycompany/OrmTests.java
@@ -1,21 +1,56 @@
 package com.mycompany;
 
+import com.mycompany.dao.Account;
 import com.mycompany.dao.User;
 import com.mycompany.executor.JdbcTemplate;
 import com.mycompany.executor.JdbcTemplateImpl;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrmTests {
 
+    private static final String URL = "jdbc:h2:mem:";
+
+    private Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL);
+        connection.setAutoCommit(false);
+        createTables(connection);
+
+        return connection;
+    }
+
+    private void createTables(Connection connection) throws SQLException {
+        PreparedStatement psUser = connection.prepareStatement(
+                "create table User(" +
+                        "id bigint(20) NOT NULL auto_increment, " +
+                        "name varchar(255), " +
+                        "age int(3))");
+        PreparedStatement psAccount = connection.prepareStatement(
+                "create table Account(" +
+                        "no bigint(20) NOT NULL auto_increment, " +
+                        "type varchar(255), " +
+                        "rest number)");
+
+        try (psUser; psAccount) {
+            psUser.executeUpdate();
+            psAccount.executeUpdate();
+        }
+    }
+
     @Test
     void checkUser() throws SQLException {
+        Connection connection = getConnection();
+
         User user = new User();
         user.setId(1L).setName("Michael").setAge(35);
-        JdbcTemplate jdbcTemplate = new JdbcTemplateImpl();
+        JdbcTemplate jdbcTemplate = new JdbcTemplateImpl(connection);
+
         jdbcTemplate.create(user);
         assertEquals(user, jdbcTemplate.load(1L, User.class),
                 "User is not saved or not loaded");
@@ -24,5 +59,27 @@ class OrmTests {
         jdbcTemplate.update(user);
         assertEquals(user, jdbcTemplate.load(1L, User.class),
                 "User is not saved or not loaded");
+
+        connection.close();
+    }
+
+    @Test
+    void checkAccount() throws SQLException {
+        Connection connection = getConnection();
+        JdbcTemplate jdbcTemplate = new JdbcTemplateImpl(connection);
+
+        Account account = new Account();
+        account.setNo(3L).setType("Personal").setRest(500);
+
+        jdbcTemplate.create(account);
+        assertEquals(account, jdbcTemplate.load(3L, Account.class),
+                "Account is not saved or not loaded");
+
+        account.setRest(450);
+        jdbcTemplate.update(account);
+        assertEquals(account, jdbcTemplate.load(3L, Account.class),
+                "Account is not saved or not loaded");
+
+        connection.close();
     }
 }
