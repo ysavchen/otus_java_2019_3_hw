4f9bce7372ea2509a4548496dbda4236c900296f
ysavchen <yura_savchenko@outlook.com>
2019-07-20 01:23:23 +0300

    added test that a user is returned from the cache

--- L21-cache/src/test/java/com/mycompany/AppWithCacheTests.java
+++ L21-cache/src/test/java/com/mycompany/AppWithCacheTests.java
@@ -11,8 +11,10 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class AppWithCacheTests {
 
@@ -46,5 +48,24 @@ class AppWithCacheTests {
                 "User is not cached");
         assertEquals(user, dbService.getUser(id).orElse(null),
                 "User is not saved or not loaded");
+    }
+
+    @Test
+    void checkUserReturnedFromCache() {
+        CacheEngine<Long, User> cache = new CacheEngineImpl.Builder(10)
+                .isEternal(true)
+                .build();
+
+        DbServiceUser dbService = Mockito.mock(DbServiceUser.class);
+        AppWithCache appWithCache = new AppWithCacheImpl(dbService, cache);
+
+        User user = new User();
+        user.setName("Michael").setAge(35);
+
+        when(dbService.saveUser(user)).thenReturn(1L);
+        long id = appWithCache.addUser(user);
+        assertEquals(user, appWithCache.getUser(id).orElse(null),
+                "User is not cached");
+        verify(dbService, times(0)).getUser(id);
     }
 }
