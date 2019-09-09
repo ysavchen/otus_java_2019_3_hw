515819576b443866158eb503a701a409261239fd
ysavchen <yura_savchenko@outlook.com>
2019-09-10 01:12:39 +0300

    updated handle functions

--- L23-web-server/src/main/resources/static/userOperations.js
+++ L23-web-server/src/main/resources/static/userOperations.js
@@ -4,16 +4,16 @@ window.onload = function () {
 }
 
 function handleAddUserButton() {
-    let name = document.getElementById("name").value;
-    let age = document.getElementById("age").value;
-    let surname = document.getElementById("surname").value;
+    let name = document.getElementById("name");
+    let surname = document.getElementById("surname");
+    let age = document.getElementById("age");
 
-    if(!Number.isInteger(age)) {
-        age = 0;
-    }
+    //if(!Number.isInteger(age)) {
+    //    age = 0;
+    //}
 
     const userStoreUrl = "http://localhost:8080/userStore";
-    let user = { name: name, surname: surname, age: age };
+    let user = { name: name.value, surname: surname.value, age: age.value };
 
     fetch(userStoreUrl, {
         method: 'POST',
@@ -29,10 +29,10 @@ function handleAddUserButton() {
             document.getElementById("infoSection").innerHTML = dataSrv;
         })
         .then(function () {
-            document.getElementById("name").value = "";
-            document.getElementById("age").value = "";
-            document.getElementById("surname").value = "";
-        })
+            name.value = "";
+            surname.value = "";
+            age.value = "";
+        });
 }
 
 function handleAllUsersButton() {
@@ -41,7 +41,43 @@ function handleAllUsersButton() {
                 return response.json();
             })
             .then(function (dataSrv) {
-                //todo: [object Object],[object Object]
-                document.getElementById("dataHolder").innerHTML = dataSrv;
-            })
+                let table = document.createElement("table");
+                table.setAttribute("id", "allUsersTable");
+
+                let head = document.createElement("thead");
+                let columns = document.createElement("tr");
+                let nameColumn = document.createElement("td");
+                nameColumn.textContent = "Name";
+                let surnameColumn = document.createElement("td");
+                surnameColumn.textContent = "Surname";
+                let ageColumn = document.createElement("td");
+                ageColumn.textContent = "Age";
+                let body = document.createElement("tbody");
+
+                for (let user of dataSrv) {
+                    let row = document.createElement("tr");
+                    let nameData = document.createElement("td");
+                    nameData.textContent = user.name;
+
+                    let surnameData = document.createElement("td");
+                    surnameData.textContent = user.surname;
+
+                    let ageData = document.createElement("td");
+                    ageData.textContent = user.age;
+
+                    row.appendChild(nameData);
+                    row.appendChild(surnameData);
+                    row.appendChild(ageData);
+                    body.appendChild(row);
+                }
+
+                columns.appendChild(nameColumn);
+                columns.appendChild(surnameColumn);
+                columns.appendChild(ageColumn);
+                head.appendChild(columns);
+                table.appendChild(head);
+                table.appendChild(body);
+
+                document.getElementById("dataHolder").appendChild(table);
+            });
 }
