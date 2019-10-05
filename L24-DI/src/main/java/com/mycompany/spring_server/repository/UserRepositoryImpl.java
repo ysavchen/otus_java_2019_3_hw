2b27a57ca08f1d3a2f87237c8b4100fd329d997a
ysavchen <yura_savchenko@outlook.com>
2019-10-05 11:42:55 +0300

    add repository annotation

--- L24-DI/src/main/java/com/mycompany/spring_server/repository/UserRepositoryImpl.java
+++ L24-DI/src/main/java/com/mycompany/spring_server/repository/UserRepositoryImpl.java
@@ -4,12 +4,14 @@ import com.mycompany.spring_server.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
+@Repository
 public class UserRepositoryImpl implements UserRepository {
 
     private final SessionFactory sessionFactory;
