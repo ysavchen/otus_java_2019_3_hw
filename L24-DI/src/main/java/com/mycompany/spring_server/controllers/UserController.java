6cbf8a0c1360fa3bcd130eda17776cb962343066
ysavchen <yura_savchenko@outlook.com>
2019-10-11 11:45:25 +0300

    remove redundant logger and .html mapping

--- L24-DI/src/main/java/com/mycompany/spring_server/controllers/UserController.java
+++ L24-DI/src/main/java/com/mycompany/spring_server/controllers/UserController.java
@@ -1,14 +1,12 @@
 package com.mycompany.spring_server.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class UserController {
 
-    @GetMapping({"/", "/userOperations", "/userOperations.html"})
+    @GetMapping({"/", "/userOperations"})
     public String userOperationsHtml() {
         return "userOperations.html";
     }
