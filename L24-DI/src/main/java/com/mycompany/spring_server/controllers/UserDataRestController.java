7b65a394adf106581b8091beffb75c0104a3db22
ysavchen <yura_savchenko@outlook.com>
2019-10-11 11:29:36 +0300

    simplify UserDataRestController

--- L24-DI/src/main/java/com/mycompany/spring_server/controllers/UserDataRestController.java
+++ L24-DI/src/main/java/com/mycompany/spring_server/controllers/UserDataRestController.java
@@ -1,11 +1,8 @@
 package com.mycompany.spring_server.controllers;
 
-import com.google.gson.Gson;
 import com.mycompany.spring_server.domain.User;
 import com.mycompany.spring_server.repository.UserRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,16 +11,13 @@ import java.util.List;
 public class UserDataRestController {
 
     private final UserRepository userRepository;
-    private final Gson gson = new Gson();
 
     public UserDataRestController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @RequestMapping(value = "/userData", method = RequestMethod.GET)
-    @ResponseBody
-    public String userData() {
-        List<User> users = userRepository.getAllUsers();
-        return gson.toJson(users);
+    @GetMapping(path = "/userData")
+    public List<User> userData() {
+        return userRepository.getAllUsers();
     }
 }
