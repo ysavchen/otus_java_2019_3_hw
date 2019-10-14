874f065b1fd29af98a09af7b7407e74db6900ede
ysavchen <yura_savchenko@outlook.com>
2019-10-14 21:49:49 +0300

    rough non-working code

--- L27-JMM/src/main/java/com/mycompany/l27/NumberSequence.java
+++ L27-JMM/src/main/java/com/mycompany/l27/NumberSequence.java
@@ -8,4 +8,34 @@ package com.mycompany.l27;
  * Поток 2: 1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1 2 3....
  */
 public class NumberSequence {
+
+    private int counter = 1;
+    private volatile boolean isPrinted1 = false;
+    private volatile boolean isPrinted2 = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        NumberSequence numberSequence = new NumberSequence();
+        numberSequence.go();
+    }
+
+    private void go() throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+        });
+        Thread thread2 = new Thread(() -> {
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+
+    private void printCounter() {
+        while (counter < 10) {
+            synchronized (this) {
+                System.out.println(Thread.currentThread().getId() + " :" + counter);
+            }
+        }
+    }
 }
