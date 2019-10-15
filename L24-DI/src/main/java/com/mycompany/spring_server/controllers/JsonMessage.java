d3f1646e172b0f8df828c7bf8146ce0b46b5f5c3
ysavchen <yura_savchenko@outlook.com>
2019-10-15 13:47:40 +0300

    fix UserStoreRestController

---
+++ L24-DI/src/main/java/com/mycompany/spring_server/controllers/JsonMessage.java
@@ -0,0 +1,10 @@
+package com.mycompany.spring_server.controllers;
+
+public class JsonMessage {
+
+    private final String message;
+
+    JsonMessage(String message) {
+        this.message = message;
+    }
+}
--- L24-DI/src/main/java/com/mycompany/spring_server/controllers/UserStoreRestController.java
+++ L24-DI/src/main/java/com/mycompany/spring_server/controllers/UserStoreRestController.java
@@ -1,10 +1,10 @@
 package com.mycompany.spring_server.controllers;
 
-import com.google.gson.Gson;
 import com.mycompany.spring_server.domain.User;
 import com.mycompany.spring_server.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserStoreRestController {
 
     private final UserRepository userRepository;
-    private final Gson gson = new Gson();
 
     public UserStoreRestController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @PostMapping(path = "/userStore")
-    public String userStore(User user) {
-        return gson.toJson(storeUser(user));
+    public JsonMessage userStore(@RequestBody User user) {
+        logger.info("User to store: " + user.toString());
+        return new JsonMessage(storeUser(user));
     }
 
     private String storeUser(User user) {
