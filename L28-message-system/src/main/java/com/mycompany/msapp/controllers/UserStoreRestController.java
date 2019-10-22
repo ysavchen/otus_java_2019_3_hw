8f82a981555751e44fb41cf33c88e157afb2d632
ysavchen <yura_savchenko@outlook.com>
2019-10-22 21:42:41 +0300

    remove ResponseBody

--- L28-message-system/src/main/java/com/mycompany/msapp/controllers/UserStoreRestController.java
+++ L28-message-system/src/main/java/com/mycompany/msapp/controllers/UserStoreRestController.java
@@ -6,7 +6,6 @@ import com.mycompany.msapp.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -21,7 +20,6 @@ public class UserStoreRestController {
     }
 
     @PostMapping(path = "/userStore")
-    @ResponseBody
     public String userStore(@RequestBody User user) {
         logger.info("User to store: " + user.toString());
         return gson.toJson(storeUser(user));
