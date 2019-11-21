0084424bacee0953a5c9c2b884b7648b2166d177
ysavchen <yura_savchenko@outlook.com>
2019-11-22 00:31:24 +0300

    update DatabaseClient

--- L30-multiprocess/src/main/java/com/mycompany/mutiprocess/database/DatabaseClient.java
+++ L30-multiprocess/src/main/java/com/mycompany/mutiprocess/database/DatabaseClient.java
@@ -1,8 +1,37 @@
 package com.mycompany.mutiprocess.database;
 
+import com.mycompany.mutiprocess.database.handlers.GetAllUsersRequestHandler;
+import com.mycompany.mutiprocess.database.handlers.StoreUserRequestHandler;
+import com.mycompany.mutiprocess.ms_client.ClientType;
+import com.mycompany.mutiprocess.ms_client.MessageType;
+import com.mycompany.mutiprocess.ms_client.MsClient;
+import com.mycompany.mutiprocess.ms_client.MsClientImpl;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class DatabaseClient {
 
     public static void main(String[] args) {
 
+    }
+
+    public SessionFactory sessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        return new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+    }
+
+    public DBService dbService(SessionFactory sessionFactory) {
+        MsClient databaseMsClient = new MsClientImpl(ClientType.DATABASE_SERVICE);
+        DBService dbService = new DBServiceImpl(sessionFactory);
+        databaseMsClient.addHandler(MessageType.STORE_USER, new StoreUserRequestHandler(dbService));
+        databaseMsClient.addHandler(MessageType.ALL_USERS_DATA, new GetAllUsersRequestHandler(dbService));
+        return dbService;
     }
 }
